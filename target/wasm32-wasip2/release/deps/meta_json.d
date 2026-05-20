488902bd9bdafd468cdcb4a8d80f7723973f2f73
Aditya <aditya.salunkh919@gmail.com>
2026-05-20 18:21:15 +0530

    feat: add meta-json stream receiver component
    Signed-off-by: Aditya <aditya.salunkh919@gmail.com>

---
+++ target/wasm32-wasip2/release/deps/meta_json.d
@@ -0,0 +1,13 @@
+/home/aditya-sal/Desktop/patch-stream/target/wasm32-wasip2/release/deps/meta_json.d: meta-json/src/lib.rs /home/aditya-sal/Desktop/patch-stream/wit/world.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-cli-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-clocks-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-filesystem-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-http-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-io-0.2.1/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-random-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-sockets-0.3.0-rc-2026-03-15/package.wit
+
+/home/aditya-sal/Desktop/patch-stream/target/wasm32-wasip2/release/deps/meta_json.wasm: meta-json/src/lib.rs /home/aditya-sal/Desktop/patch-stream/wit/world.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-cli-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-clocks-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-filesystem-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-http-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-io-0.2.1/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-random-0.3.0-rc-2026-03-15/package.wit /home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-sockets-0.3.0-rc-2026-03-15/package.wit
+
+meta-json/src/lib.rs:
+/home/aditya-sal/Desktop/patch-stream/wit/world.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-cli-0.3.0-rc-2026-03-15/package.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-clocks-0.3.0-rc-2026-03-15/package.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-filesystem-0.3.0-rc-2026-03-15/package.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-http-0.3.0-rc-2026-03-15/package.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-io-0.2.1/package.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-random-0.3.0-rc-2026-03-15/package.wit:
+/home/aditya-sal/Desktop/patch-stream/wit/deps/wasi-sockets-0.3.0-rc-2026-03-15/package.wit:
